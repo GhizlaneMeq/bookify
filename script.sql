22b470a73beb3b237b5a0dbd15297b1fae1f1966
GhizlaneMeq <meqdarghizlane@gmail.com>
2023-11-20 09:52:04 +0100

    affichage des données

--- script.sql
+++ script.sql
@@ -67,7 +67,7 @@ ADD CONSTRAINT unique_email UNIQUE (email);
 
 -- insertion des données 
 
-INSERT INTO departements (nom) VALUES ('IT'), ('HR'), ('clients service');
+INSERT INTO departements (nom) VALUES ('IT'), ('HR'), ('clients service'),('Marketing'),('Comptabilité');
 
 INSERT INTO postes (nom) VALUES ('Manager'), ('Developer'), ('Designer');
 
@@ -79,9 +79,24 @@ INSERT INTO employees (nom, email, departement_id, poste_id) VALUES
 
 INSERT INTO salles (nom, capacite, departement_id) VALUES
 ('Meeting Room', 20, 1),
-('Conference Room', 30, 2);
+('Conference Room', 30, 2),
+('Breaking code', 30, 1),
+('Agora', 140, 3);
 
-INSERT INTO equipements (nom, type) VALUES ('Laptop', 'changeable'), ('Projector', 'non changeable');
+INSERT INTO equipements (nom, type) VALUES 
+('Laptop', 'changeable'), 
+('Projector', 'non changeable'),
+('remote', 'changeable'),
+('Table', 'non changeable'),
+('HDMI', 'non changeable'),
+('chaise','non changeable');
+
+INSERT INTO reservations (date_debut, date_fin, employe_id, salle_id)
+VALUES ('2023-11-20','2023-11-20',1,2),
+('2023-11-21','2023-11-21',2,4),
+('2023-12-22','2023-12-22',3,4),
+('2023-11-23','2023-11-23',4,1),
+('2024-01-01','2024-01-01',2,3);
 
 -- affichage des données
 
@@ -89,7 +104,14 @@ select * from employees;
 select * from reservations;
 
 SELECT * FROM salles WHERE id =2 ;
+SELECT * FROM employees LIMIT 2;
 SELECT * FROM equipements WHERE type = 'changeable';
+SELECT * FROM employees WHERE nom LIKE 'g%';
+SELECT * FROM equipements WHERE nom IN ('Table', 'Laptop');
+
+SELECT nom FROM salles WHERE capacite BETWEEN 20 AND 80;
+
+SELECT SUM(capacite) FROM salles ;
 
 SELECT employees.nom AS employee_name, 
 departements.nom AS department_name
@@ -102,4 +124,20 @@ salles.nom AS room_name
 FROM reservations
 INNER JOIN salles 
 ON reservations.salle_id = salles.id;
+
+SELECT * FROM reservations
+WHERE date_debut BETWEEN '2023-11-21' AND '2023-11-24';
+
+SELECT
+    employees.id AS employee_id,
+    employees.nom AS employee_name,
+    COUNT(reservations.id) AS total_reservations
+FROM employees
+JOIN reservations ON employees.id = reservations.employe_id
+GROUP BY employees.id
+HAVING total_reservations > 1
+ORDER BY total_reservations
+DESC
+    ;
+
 
